682210b065b59de1cd164b6dcaebe47b40e4bc2d
thmscode <102998926+thmscode@users.noreply.github.com>
2023-03-03 22:43:40 -0500

    Set up routes
    Application currently renders very basic components when their corresponding routes are hit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,10 +4,15 @@ import {
   Route,
   RouterProvider
 } from 'react-router-dom';
-import Root from './layouts/Root'
+import Root from './layouts/Root';
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<Root />}>
+    <Route index element={<div>Homepage</div>} />
+    <Route path="meals_random" element={<div>Random Meal Recipe!</div>} />
+    <Route path="drinks_random" element={<div>Random Drink Recipe!</div>} />
+    <Route path="signin" element={<div>Sign in page</div>} />
+    <Route path="signup" element={<div>Sign up page</div>} />
     <Route path="*" element={<div>That page does not exist!</div>} />
   </Route>
 ));
