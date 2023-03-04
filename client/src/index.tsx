0eaff3a12c475adc365dd7a0becb992a54950b59
thmscode <102998926+thmscode@users.noreply.github.com>
2023-03-03 22:31:23 -0500

    Update index.tsx
    Added new colours for theme purposes.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  colors: {
+    redwood: {
+      200: '#BB4430',
+      400: '#A23E48'
+    },
+    xanthous: {
+      200: '#FFE066',
+      400: '#FCBF49'
+    }
+  }
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -9,7 +22,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
