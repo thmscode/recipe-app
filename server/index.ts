158ab1030f1af6155922b89a2091368ef9d01456
thmscode <102998926+thmscode@users.noreply.github.com>
2023-03-06 17:06:16 -0500

    Install TypeScript
    Installed TS and type definition packages for Express & Node.
    
    Specified outDir of tsconfig to be /dist. Where the TS -> JS transpiled code will be found.

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,8 @@
 require('dotenv').config();
+import express, { Express, Request, Response } from 'express';
+
 const PORT = process.env.PORT;
-const express = require('express');
-const app = express();
+const app: Express = express();
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
