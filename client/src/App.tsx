776b62564453c7151fe8332dbdbb5f815b74118f
thmscode <102998926+thmscode@users.noreply.github.com>
2023-03-13 18:12:06 -0400

    Update App.tsx
    Added a page component that gets rendered when the 'random_drink' route is accessed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,13 +5,14 @@ import {
   RouterProvider
 } from 'react-router-dom';
 import Root from './layouts/Root';
+import RandomDrink from './pages/RandomDrink';
 import RandomMeal from './pages/RandomMeal';
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<Root />}>
     <Route index element={<div>Homepage</div>} />
     <Route path="random_meal" element={<RandomMeal />} />
-    <Route path="random_drink" element={<div>Random Drink Recipe!</div>} />
+    <Route path="random_drink" element={<RandomDrink />} />
     <Route path="login" element={<div>Login page</div>} />
     <Route path="signup" element={<div>Sign up page</div>} />
     <Route path="*" element={<div>That page does not exist!</div>} />
