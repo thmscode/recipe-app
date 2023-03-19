8acc593ab9a5eb957d20cc4ba62a917a9af6acda
thmscode <102998926+thmscode@users.noreply.github.com>
2023-03-18 23:25:41 -0400

    Update Root.tsx
    Removed Footer component from the Root layout

--- client/src/layouts/Root.tsx
+++ client/src/layouts/Root.tsx
@@ -1,14 +1,12 @@
 import { Flex } from '@chakra-ui/react';
 import { Outlet } from 'react-router-dom';
-import Navbar from '../components/navbar/Navbar'
-import Footer from '../components/footer/Footer';
+import Navbar from '../components/navbar/Navbar';
 
 const Root = () => {
   return (
     <Flex direction={'column'} minH={'100vh'}>
       <Navbar />
       <Outlet />
-      <Footer />
     </Flex>
   );
 }
