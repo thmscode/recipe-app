7b33a816c8b93270f5441d597529f87e7eac0a50
thmscode <102998926+thmscode@users.noreply.github.com>
2023-03-25 14:39:12 -0400

    Establish connection to local mongodb

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,21 @@
 require('dotenv').config();
 import express, { Express } from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
 import countriesRouter from './routes/countries';
 import categoriesRouter from './routes/categories';
 import recipesRouter from './routes/recipes';
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT ?? 5000;
+const DATABASE_URL = process.env.DATABASE_URL ?? 'mongodb://127.0.0.1:27017/recipeAppDB';
 const app: Express = express();
+
+mongoose.connect(DATABASE_URL)
+  .then(() => console.log('MONGO CONNECTION ESTABLISHED'))
+  .catch(e => {
+    console.log('MONGO CONNECTION FAILED');
+    console.log(e);
+  });
 
 app.use(express.json());
 app.use(cors());
