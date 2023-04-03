468529bfcaa284dfb61b7f0635a76349a1ab7e6a
thmscode <102998926+thmscode@users.noreply.github.com>
2023-04-03 19:16:46 -0400

    Add Footer component to the layout
    Giving credit to TheMealDB & TheCocktailDB for their APIs which the site was built on

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -7,19 +7,11 @@ import {
 
 const Footer = () => {
   return (
-    <Flex
-      minH={'50px'}
-      w={'100%'}
-      position={'absolute'}
-      bottom={0}
-      align={'center'}
-      justify={'center'}>
-      <Box>
-        <Text>
-          Powered by <Link href={'https://www.themealdb.com/'} color={'redwood.400'} >TheMealDB</Link> & <Link href={'https://www.thecocktaildb.com/'} color={'redwood.400'}>TheCocktailDB</Link>
-        </Text>
-      </Box>
-    </Flex>
+    <Box py={3} mt={'auto'}>
+      <Text align={'center'}>
+        Powered by <Link href={'https://www.themealdb.com/'} color={'redwood.400'} >TheMealDB</Link> & <Link href={'https://www.thecocktaildb.com/'} color={'redwood.400'}>TheCocktailDB</Link>
+      </Text>
+    </Box>
   );
 }
 
--- client/src/layouts/Root.tsx
+++ client/src/layouts/Root.tsx
@@ -1,12 +1,14 @@
 import { Flex } from '@chakra-ui/react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/navbar/Navbar';
+import Footer from '../components/footer/Footer';
 
 const Root = () => {
   return (
     <Flex direction={'column'} minH={'100vh'}>
       <Navbar />
       <Outlet />
+      <Footer />
     </Flex>
   );
 }
