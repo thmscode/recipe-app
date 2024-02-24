7292a9b71bc2bf5b6d92ad6c9415b1aac6be94fd
thmscode <102998926+thmscode@users.noreply.github.com>
2024-02-23 21:53:36 -0500

    Update helperFn.ts

--- server/utils/helperFn.ts
+++ server/utils/helperFn.ts
@@ -1,19 +1,14 @@
-import {
-  APIDrinkRecipe,
-  APIMealRecipe,
-  DrinkRecipe,
-  MealRecipe,
-} from "./types";
+import { DrinkRecipe, MealRecipe, Drink, Meal } from "./types";
 
-const formatIngredients = (recipe: APIMealRecipe | APIDrinkRecipe): string[] => {
+const formatIngredients = (recipe: MealRecipe | DrinkRecipe): string[] => {
   const items: string[] = [];
   const measurements: string[] = [];
   const ingredients: string[] = [];
 
   for (const [key, value] of Object.entries(recipe)) {
-    if (key.includes('strIngredient') && (value !== '' && value !== null && value !== ' ')) items.push(value);
-    if (key.includes('strMeasure') && (value !== null)) measurements.push(value);
-    if (key.includes('strMeasure') && (value === null)) measurements.push('');
+    if (key.includes("strIngredient") && value !== "" && value !== null && value !== " " ) items.push(value);
+    if (key.includes("strMeasure") && value !== null) measurements.push(value);
+    if (key.includes("strMeasure") && value === null) measurements.push("");
   }
 
   for (let i = 0; i < items.length; i++) {
@@ -24,34 +19,34 @@ const formatIngredients = (recipe: APIMealRecipe | APIDrinkRecipe): string[] => 
   return ingredients;
 };
 
-export const formatMealResponse = (recipe: APIMealRecipe): MealRecipe => {
+export const formatMealResponse = (recipe: MealRecipe): Meal => {
   const ingredients = formatIngredients(recipe);
 
-  const formattedRecipe: MealRecipe = {
-    "id": recipe.idMeal,
-    "title": recipe.strMeal,
-    "category": recipe.strCategory,
-    "area": recipe.strArea,
-    "instructions": recipe.strInstructions,
-    "thumbnail": recipe.strMealThumb,
-    "youtubeUrl": recipe.strYoutube,
-    ingredients
-  }
+  const formattedRecipe: Meal = {
+    id: recipe.idMeal,
+    title: recipe.strMeal,
+    category: recipe.strCategory,
+    area: recipe.strArea,
+    instructions: recipe.strInstructions,
+    thumbnail: recipe.strMealThumb,
+    youtubeUrl: recipe.strYoutube,
+    ingredients,
+  };
   return formattedRecipe;
 };
 
-export const formatDrinkResponse = (recipe: APIDrinkRecipe): DrinkRecipe => {
+export const formatDrinkResponse = (recipe: DrinkRecipe): Drink => {
   const ingredients = formatIngredients(recipe);
 
-  const formattedRecipe: DrinkRecipe = {
-    "id": recipe.idDrink,
-    "title": recipe.strDrink,
-    "category": recipe.strCategory,
-    "alcoholic": recipe.strAlcoholic,
-    "glass": recipe.strGlass,
-    "instructions": recipe.strInstructions,
-    "thumbnail": recipe.strDrinkThumb,
-    ingredients
-  }
+  const formattedRecipe: Drink = {
+    id: recipe.idDrink,
+    title: recipe.strDrink,
+    category: recipe.strCategory,
+    alcoholic: recipe.strAlcoholic,
+    glass: recipe.strGlass,
+    instructions: recipe.strInstructions,
+    thumbnail: recipe.strDrinkThumb,
+    ingredients,
+  };
   return formattedRecipe;
 };
